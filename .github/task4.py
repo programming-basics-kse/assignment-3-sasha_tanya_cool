cf32df6145a76bd3c4efb66f1b819d322249c34c
ovalova <ovalova@kse.org.ua>
2024-11-28 21:32:15 +0200

    worst year

--- .github/task4.py
+++ .github/task4.py
@@ -8,4 +8,13 @@ def find_best_year(data, country):
             max_medals = total_medals
             best_year = year
     return best_year
-
+def find_worst_year(data, country):
+    worst_year = None
+    min_medals = 0
+    for year in set(row['Year'] for row in data if row['Team'] == country):
+        medals = medals(data, country, year)
+        total_medals = sum(medals.values())
+        if total_medals < min_medals:
+            min_medals = total_medals
+            worst_year = year
+    return worst_year

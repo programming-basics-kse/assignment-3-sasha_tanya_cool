80221eba6032edc552cbdbaff8c442164f03b5d0
ovalova <ovalova@kse.org.ua>
2024-11-29 23:45:31 +0200

    aditional correct mistakes

--- plus_task.py
+++ plus_task.py
@@ -8,14 +8,14 @@
             weight = float(row[WEIGHT]) if row[WEIGHT].replace(".", "").isdigit() else None
             height = float(row[HEIGHT]) if row[HEIGHT].replace(".", "").isdigit() else None
 
-            if age_group and (age is None or not (age_ranges[age_group][0] <= age < age_ranges[age_group][1])):
-            continue
+            if age_kategor and (age is None or not (age_ranges[age_kategor][0] <= age < age_ranges[age_kategor][1])):
+                continue
 
-        if weight_group and (weight is None or not (weight_ranges[weight_group][0] <= weight < weight_ranges[weight_group][1])):
-            continue
+            if weight_kategor and (weight is None or not (weight_ranges[weight_kategor][0] <= weight < weight_ranges[weight_kategor][1])):
+                continue
 
-        if height_group and (height is None or not (height_ranges[height_group][0] <= height < height_ranges[height_group][1])):
-            continue
+            if height_kategor and (height is None or not (height_ranges[height_kategor][0] <= height < height_ranges[height_kategor][1])):
+                continue
 
         if row[MEDAL] != "NA":
             medal_score = {"Gold": 5, "Silver": 3, "Bronze": 1}[row[MEDAL]]
@@ -28,11 +28,8 @@ parser.add_argument("-height", help="Height group (1-4)")
 elif args.top:
     top_n = int(args.top[0])
     gender = args.top[1]
-    age_group = args.a
-    weight_group = args.w
-    height_group = args.height  # Заміна args.h на args.height
-    top_players(file, top_n, gender, age_group, weight_group, height_group)
-#     top_players = sorted(players.items(), key=lambda  x: x[1], reverse=True)[:top_n]
-#
-#
+    age_kategor = args.a
+    weight_kategor = args.w
+    height_kategor = args.height  # Заміна args.h на args.height
+    top_players(file, top_n, gender, age_kategor, weight_kategor, height_kategor)
 
